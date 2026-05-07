64d478fc9a85a338d4504370bf4d3bb693b6bc90
Ratul Islam <traderspopy@gmail.com>
2026-05-08 05:30:42 +0600

    Remove remaining test-app directory

--- test-app/src/biokey.js
+++
@@ -1,5 +0,0 @@
-// This file is intentionally empty.
-// The local biokey implementation has been replaced by the published npm package.
-// Import from 'biokey-js' instead.
-//
-// See: https://www.npmjs.com/package/biokey-js
